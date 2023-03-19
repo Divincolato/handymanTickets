eed9eb5774d534bfb46358364e7db6c3b523ecea
Nicolò <n.berti@mail.com>
2023-03-19 23:29:12 +0100

    login e visualizzazione diversa per ogni persona

---
+++ lavoratore.js
@@ -0,0 +1,27 @@
+let tickets = { push: function push(element) { [].push.call(this, element) } };
+tickets = JSON.parse(window.localStorage.getItem("tickets"));
+
+let username = window.localStorage.getItem("username");
+  // Seleziona l'elemento DOM in cui vuoi riempire i dati
+  const ticketList = document.querySelector("#ticket-list");
+
+  // Cicla attraverso l'array di ticket e crea una riga della tabella per ogni ticket
+  tickets.forEach((ticket) => {
+    if (username == ticket.lavoratore||username == "admin"){
+    // Crea una nuova riga della tabella
+    const tableRow = document.createElement("tr");
+
+    // Aggiungi i dettagli del ticket alla riga della tabella
+    const ticketDetails = `
+      <td>${ticket.tipoIntervento}</td>
+      <td>${ticket.data}</td>
+      <td>${ticket.lavoratore}</td>
+      <td>${ticket.oreImpiegate}</td>
+      <td>${ticket.materialiImpiegati.join(", ")}</td>
+      <td>${ticket.completato ? "Si" : "No"}</td>
+    `;
+    tableRow.innerHTML = ticketDetails;
+
+    // Aggiungi la riga della tabella alla lista di ticket
+    ticketList.appendChild(tableRow);}
+  });
